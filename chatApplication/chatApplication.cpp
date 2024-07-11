a2a0559ae80e2105d3f51f755a8bbbb19fe64f06
Droffilc13 <62885511+Droffilc13@users.noreply.github.com>
2024-07-11 16:33:31 +0800

    Add project files.

---
+++ chatApplication/chatApplication.cpp
@@ -0,0 +1,12 @@
+﻿// chatApplication.cpp : Defines the entry point for the application.
+//
+
+#include "chatApplication.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
